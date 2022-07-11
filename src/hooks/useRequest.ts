fdc95f76277862cd11a56ef214440506e537c76f
DYH <xinzon32177@163.com>
2022-07-11 18:03:34 +0800

    feat(hooks): useRequest 添加自动请求功能

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,4 +1,4 @@
-import { reactive, toRefs } from 'vue';
+import { reactive, toRefs, isReactive, watch, isRef } from 'vue';
 // import User from '@/api/User';
 
 type FN = (...args: any[]) => Promise<any>;
@@ -9,35 +9,93 @@ interface State<T extends FN> {
   error: any | null;
 }
 
-export function useRequest<T extends FN>(fn: T) {
-  const state = reactive<State<T>>({
+type Options<A extends string, D extends object | void = void> = D extends void
+  ? { requestAlias?: A; immediate?: boolean }
+  : {
+      immediate?: boolean;
+      data?: D;
+      dataDriver?: boolean;
+    };
+
+export function useRequest<
+  REQ extends FN,
+  ALIAS extends string = 'request',
+  DATA extends object | void = void,
+>(requestFn: REQ, options: Options<ALIAS, DATA> = {} as any) {
+  const state = reactive<State<REQ>>({
     loading: false,
     data: null,
     error: null,
   });
 
-  return {
-    ...toRefs(state),
-    request(...args: Parameters<T>) {
-      state.loading = true;
-      fn(...args)
-        .then(
-          (res) => {
-            state.data = res.data;
-          },
-          (err) => (state.error = err),
-        )
-        .finally(() => {
-          state.loading = false;
-        });
-    },
+  const refs = toRefs(state);
+
+  const request = (...args: any) => {
+    state.loading = true;
+    requestFn(...args)
+      .then(
+        (res) => {
+          state.data = res.data;
+        },
+        (err) => (state.error = err),
+      )
+      .finally(() => {
+        state.loading = false;
+      });
   };
+
+  const {
+    requestAlias = 'request',
+    immediate = false,
+    data,
+    dataDriver = false,
+  } = options as Options<ALIAS, {}> & Options<ALIAS>;
+
+  // 数据驱动
+  if (dataDriver && data && (isReactive(data) || isRef(data))) {
+    watch(
+      data,
+      (n) => {
+        request(n);
+      },
+      { deep: true },
+    );
+  }
+
+  if (immediate) {
+    request(data);
+  }
+
+  return {
+    ...refs,
+    // 数据驱动时as any一下还是能用的
+    [requestAlias]: request,
+  } as typeof refs &
+    (DATA extends void
+      ? { [k in keyof Record<ALIAS, void>]: (...args: Parameters<REQ>) => void }
+      : void);
 }
 
-// const res = useRequest(User.getSelf);
-// res.request();
+// // 手动请求 request不带参数
+// const res = useRequest(User.getSelf, { requestAlias: 'getSelf', immediate: true });
+// res.getSelf();
 // console.log(res.data.value?.user);
 //
+// const formModel = reactive({ username: '', password: '' });
+//
+// // 手动请求 request带参数
 // const res2 = useRequest(User.login);
-// res2.request({ username: '', password: '' });
+// res2.request(formModel);
 // console.log(res2.data.value?.token);
+//
+// formModel.username = '1';
+// formModel.password = '1';
+//
+// // 数据驱动
+// const res3 = useRequest(User.login, {
+//   data: formModel,
+//   immediate: true,
+//   dataDriver: true,
+// });
+// // res3.request(formModel); // error Property 'request' does not exist
+// console.log(res3.data.value?.token);
